23a4c00e2e0467f23e745749eafda30514378fea
Jacob McFadden <jtm429@gmail.com>
2018-11-13 08:11:00 -0600

    midinote added

---
+++ src/mutheory/Midinote.java
@@ -0,0 +1,21 @@
+package mutheory;
+
+
+public class Midinote {
+	
+	//what note the note plays
+	Miditone myNote;
+	//1 = whole note
+	//2 = half note
+	//4 = quarter note
+	//8 = eighth note
+	//etc.
+	int len;
+	
+	
+	
+	
+	
+	
+	
+}
